6876c88749e5e818812f64e775f801bfd4d04a97
ka-me-i <126389688+ka-me-i@users.noreply.github.com>
2023-06-01 11:06:28 +0900

    公開

--- js/script.js
+++ js/script.js
@@ -123,6 +123,33 @@ $(function () {
         });
     });
 });
+// フェードインアニメーションここまで
+
+// タッチデバイスでのhover解除
+var touch =
+    "ontouchstart" in document.documentElement ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0;
+
+if (touch) {
+    // remove all :hover stylesheets
+    try {
+        // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(":hover")) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
+// タッチデバイスでのhover解除ここまで
 
 // はじめに部分のスライダー
 $(function () {
